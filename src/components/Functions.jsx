0979efe3aec53e9eb2638866864ecf3213837435
FerMossier <mossierfernando@gmail.com>
2021-10-04 13:25:25 -0300

    Se modifica un poco el estilo

--- src/components/Functions.jsx
+++ src/components/Functions.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 const Functions = ({onContentClear, onDelete}) => (
     <section className="functions">
-        <Button type = "button-long-text" text = "Clear" clickHandler = {onContentClear}/>
+        <Button type = "button-long-text" text = "AC" clickHandler = {onContentClear}/>
         <Button text = "⌫" clickHandler = {onDelete}/>
     </section>
 )
